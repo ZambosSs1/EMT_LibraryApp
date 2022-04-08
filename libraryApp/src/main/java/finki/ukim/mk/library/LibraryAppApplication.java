48611330d7cee2a98cc20da1b947ab6b1e6134d8
Bojan Simichiev <bojansimiciev@gmail.com>
2022-04-08 17:13:32 +0200

    DataBase Init

--- libraryApp/src/main/java/finki/ukim/mk/library/LibraryAppApplication.java
+++ libraryApp/src/main/java/finki/ukim/mk/library/LibraryAppApplication.java
@@ -2,8 +2,11 @@ package finki.ukim.mk.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ServletComponentScan
 public class LibraryAppApplication {
 
     public static void main(String[] args) {
